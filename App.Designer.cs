9ccca511fcc5e1eb612c2ad16447f9fedb797ecf
quoctan342 <69149606+quoctan342@users.noreply.github.com>
2021-01-17 22:56:51 +0700

    Giao diện và code màn hình chính của phần mềm
    File giao diện và màn hình chính của phần mềm

---
+++ App.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace QuanLyKhachSan
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnHDTT = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(90, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(481, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ỨNG DỤNG QUẢN LÝ KHÁCH SẠN";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(65, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Lập danh mục phòng";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(365, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(265, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Lập phiếu thuê phòng";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(65, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(265, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Tra cứu phòng";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btnHDTT
+            // 
+            this.btnHDTT.BackColor = System.Drawing.Color.Transparent;
+            this.btnHDTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHDTT.Location = new System.Drawing.Point(365, 153);
+            this.btnHDTT.Name = "btnHDTT";
+            this.btnHDTT.Size = new System.Drawing.Size(265, 47);
+            this.btnHDTT.TabIndex = 4;
+            this.btnHDTT.Text = "Lập hóa đơn thanh toán";
+            this.btnHDTT.UseVisualStyleBackColor = false;
+            this.btnHDTT.Click += new System.EventHandler(this.btnHDTT_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(65, 232);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(265, 47);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Lập báo cáo tháng";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(365, 232);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(265, 47);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Thay đổi qui định";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.Transparent;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(212, 312);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(265, 47);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 395);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnHDTT);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "App";
+            this.Text = "Quản lý khách sạn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnHDTT;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
